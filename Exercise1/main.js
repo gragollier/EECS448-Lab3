f7c4330898a979c62ceff244a2c856ca822c5d87
Grant Gollier <gragollier@gmail.com>
2019-09-29 14:56:20 -0500

    Change password validation method and add message for matching passwords

--- Exercise1/main.js
+++ Exercise1/main.js
@@ -1,9 +1,11 @@
 function validatePassword() {
-    const password1 = document.getElementById('password1').textContent;
-    const password2 = document.getElementById('password2').textContent;
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
     if (password1 !== password2) {
         alert("Entered passwords don't match!");
     } else if (password1.length < 8 || password2.length < 8) {
         alert("Passwords must be at least 8 characters!");
+    } else if (password1 === password2) {
+        alert("Passwords Match!");
     }
 }
